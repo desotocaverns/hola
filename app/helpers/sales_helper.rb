3fbebdadfcb627839279565e9154f49c66f22dc6
Brandon Mathis <brandon@imathis.com>
2015-09-09 23:58:25 -0500

    More styling improvements #39

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -6,4 +6,15 @@ module SalesHelper
   def collect_quantity_method
     @sale.new_record? ? :create : :patch
   end
+  def cart_items
+    @sale.purchases.map(&:quantity).reduce(:+)
+  end
+
+  def cart_packages
+    @sale.purchases.where(type: PackagePurchase)
+  end
+
+  def cart_tickets
+    @sale.purchases.where(type: TicketPurchase)
+  end
 end
