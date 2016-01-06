98ddeab38568557279f00881054b320734e48543
ijwilliams <isaac@thewilliams.ws>
2016-01-06 16:47:54 -0500

    Use Rails 4 inline attachments for QR code in email

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,14 +1,19 @@
 class CustomerMailer < ApplicationMailer
   default from: "info@desotocavernspark.com"
-  
+
   add_template_helper(ApplicationHelper)
 
   def receipt_email(sale, protohost)
     @sale = sale
 
     io = StringIO.new
-    @sale.redemption_qrcode(protohost).as_png.write(io)
+    @sale.redemption_qrcode(protohost).as_png(size: 800, border_modules: 2).write(io)
     io.rewind
+
+    attachments.inline["redemption_qrcode.png"] = {
+      :mime_type => 'image/png',
+      :content => io.read
+    }
 
     mail(to: @sale.email, from: Settings[:company_email], subject: 'DeSoto Caverns receipt')
   end
