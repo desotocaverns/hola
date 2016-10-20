3478481b303924cad16859ed023b5b1421db9f73
ijwilliams <isaac@thewilliams.ws>
2016-10-20 13:52:01 -0400

    Try another logging method for older Rails versions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   config.assets.compile = true
   config.assets.digest = true
   config.log_level = :debug
-  config.log_tags = [ :request_id ]
+  config.log_tags = [ :uuid ]
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
