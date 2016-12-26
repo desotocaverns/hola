f9de76392634b4bd9b484a535a58097f5af393c8
Isaac Williams <isaac@thewilliams.ws>
2016-12-26 17:22:51 -0500

    Don't let customers buy 0 items (#80)

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -6,6 +6,8 @@ class Purchase < ActiveRecord::Base
   before_create :generate_unique_token
   before_save :calculate_expiration_date # Defined by children
 
+  validate :quantity_non_zero
+
   # Answers the name of the Purchase item at the time the Purchase was created.
   def name
     revision_data["name"]
@@ -19,5 +21,11 @@ class Purchase < ActiveRecord::Base
 
   def generate_unique_token
     self.token = SecureRandom.urlsafe_base64(10) + self.id.to_s
+  end
+
+  def quantity_non_zero
+    if !self.quantity || self.quantity <= 0
+      errors.add(:quantity, "can't be zero")
+    end
   end
 end
